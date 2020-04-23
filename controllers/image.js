6f835f7a6220faeacda4ff8242364041f8354f01
mmertdogann <doganme@mef.edu.tr>
2020-04-24 02:16:12 +0300

    update

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'e1ab6b06d26448c0880ba4666306244b'
 });
 
 const handleApiCall = (req, res) => {
